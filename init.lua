767635004f2eb05028c600e86cbda350fbc9fbec
Hugo <hugo.pm@protonmail.com>
2023-10-23 17:08:07 +0200

    leader and merlin

--- init.lua
+++ init.lua
@@ -40,8 +40,8 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -582,3 +582,7 @@ vim.o.relativenumber = true
 
 --vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+local handle = io.popen("opam var share")
+vim.g.opamshare = handle:read("*a"):gsub('[\n\r]', '')
+vim.opt.rtp:append { vim.g.opamshare .. "/merlin/vim" }
+handle:close()
